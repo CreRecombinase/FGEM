5871c4a44eb298ea5b2e371ab5ad303aa9a1c570
Nicholas Wilson Knoblauch <nwknoblauch@spudhead.localdomain>
2015-06-18 10:58:13 -0500

    About to be changed to accommodate new parameters for FGEM

--- Submit_PPS.pl
+++ Submit_PPS.pl
@@ -2,19 +2,39 @@
 use strict;
 use warnings;
 
-my $homedir = $ARGV[0];
-my $shelldir = $ARGV[1];
-my $GOfile = $ARGV[2];
-my $BFfile= $ARGV[3];
-my $startfeat = $ARGV[4];
-my $lastfeat = $ARGV[5];
-my $featsize =$ARGV[6];
 
-my @array = ($startfeat .. $lastfeat);
+my $shelldir = $ARGV[0];
+my $startfeat = $ARGV[1];
+my $lastfeat = $ARGV[2];
+my $featsize =$ARGV[3];
 
-for{ my $item =$startfeat; $item<$lastfeat; $item+=$featsize){
+
+
+
+for( my $item = $startfeat; $item<$lastfeat; $item+=$featsize){
+    
     my $tstop = $item+$featsize;
-    my $shellfile = "${shelldir}/FGEM_${item}_${tstop}";
+    if($tstop> $lastfeat){
+	$tstop = $lastfeat;
+    }
+    my $shellbase = "${shelldir}/FGEM_${item}_${tstop}";
+    my $shellout = "${shellbase}.out";
+    my $shellerr = "${shellbase}.err";
+    my $shellfile = "${shellbase}.sh";
     print $shellfile."\n";
-}
-	
+
+    unless (-e $shellfile){
+	open(SCRIPT,">${shellfile}");
+	print SCRIPT "#!/bin/sh\n";
+	print SCRIPT "#\$ -o ${shellout}\n";
+	print SCRIPT "#\$ -e ${shellerr}\n";
+	print SCRIPT "#\$ -l h_vmem=1g\n";
+	print SCRIPT "/mnt/gluster/home/nwknoblauch/bin/Rscript /mnt/gluster/home/nwknoblauch/Gene_Annotation/FGEM/Bayes_EM_GWAS.R /mnt/gluster/home/nwknoblauch/Gene_Annotation/GOmat.RDS /mnt/gluster/home/nwknoblauch/Gene_Annotation/TADA_ASC_SSC_results_Dec23.csv ${item} ${featsize} /mnt/gluster/home/nwknoblauch/Gene_Annotation/ \n";
+	close(SCRIPT);
+	system("qsub ${shellfile}");
+	print "${item}\n";
+	sleep(1);
+
+    
+    }
+}	
