6fc497dbac4dc07f714ff31418718074d3c69854
Nicholas Knoblauch <nwknoblauch@gmail.com>
2016-03-15 15:13:04 -0500

    New code to run EM with SQUAREM, and option of using Bayesian Logistic Regression (currently defaults to yes with no programmatic way to turn it off)

--- Bayes_EM_GWAS.R
+++ Bayes_EM_GWAS.R
@@ -1,32 +1,39 @@
 library(methods)
 library(Matrix)
 library(plyr)
-library(doMC)
-nodes <- detectCores()
+library(SQUAREM)
+library(arm)
+# #nodes <- detectCores()
 # 
-registerDoMC(nodes)
-                                        
-GOmatfile <- "BrainSpanVar.RDS"
-BayesFactorFile <- BayesFactorFile
-Startterm <- 1
-Numterms <- 15
-EMiter <- 50
-featureClassName <- "BrainExpressionVar"
-Outdir <- "./"
+# registerDoMC(nodes)
+Exacff <- "~/VariantPrioritization/gene_exac.RDS"
+Ex <- readRDS(Exacff)
+rownames(Ex) <- Ex$gene
+tallgenes <- allgenes[rownames(allgenes) %in% rownames(Ex),]
+tEx <- Ex[rownames(tallgenes),]
+tB <- tallgenes$B
+         
+ #GOmatfile <- "/mnt/gluster/home/nwknoblauch/Gene_Annotation/GOmat.RDS"
+ #BayesFactorFile <- "/mnt/gluster/home/nwknoblauch/Gene_Annotation/TADA_ASC_SSC_results_Dec23.csv"
+ #Startterm <- 1
+ #Numterms <- 3
+
+# Outdir <- "/mnt/gluster/home/nwknoblauch/FGEM_GO_RDS/"
 #Read from the command line to get which iteration to use
 args <- commandArgs(trailingOnly=T)
 GOmatfile <- args[1]
 BayesFactorFile <- args[2]
 Startterm <- as.integer(args[3])
 Numterms <- as.integer(args[4])
-EMiter <- as.integer(args[5])
-Outdir <- args[6]
+Outdir <- args[5]
 
-outfile <- file.path(Outdir,paste0("FGEM_DF_",featureClassName,"_",Startterm,"_",Startterm+Numterms,".RDS"))
+outfile <- file.path(Outdir,paste0("FGEM_DF_",Startterm,"_",Startterm+Numterms,".RDS"))
 if(!all(file.exists(c(GOmatfile,BayesFactorFile)))){
     stop(paste0("File not found: ",c(GOmatfile,BayesFactorFile)[!file.exists(c(GOmatfile,BayesFactorFile))]))
 }
 GOmat <- readRDS(GOmatfile)
+print("Size of Annotation Matrix is ")
+print(dim(GOmat))
 BF.df <- read.table(BayesFactorFile,header=T,sep=",")
 
 
@@ -44,10 +51,10 @@ B <- allgenes[rownames(GOmat),"B"]
 
 ####Starting EM
 #Computing the null model
-likfun <- function(x,B){
-  sum(log((1/(1+exp(-x)))*B+(1-(1/(1+exp(-x))))))
-}
-NullLogLikelihood <- optimize(likfun,interval = c(-10,10),B=B,maximum=T)[["objective"]]
+## likfun <- function(x,B){
+#  sum(log((1/(1+exp(-x)))*B+(1-(1/(1+exp(-x))))))
+#}
+#NullLogLikelihood <- optimize(likfun,interval = c(-10,10),B=B,maximum=T)[["objective"]]
 
 #If the number of terms to be tested is greater than the number of remaining terms
 # we change the number of terms to be tested
@@ -61,123 +68,66 @@ GOmat <- GOmat[,Startterm:(Startterm+Numterms)]
 print("GO matrix is now of size ")
 print(dim(GOmat))
 
-
-#Function that performs EM
-FGEM <- function(x,B,Z=NULL,iters=NULL,tol=NULL,keep=F,NullLogLikelihood=NULL){
-  #x is a vector of length I with relevant annotations
-  #B is a vector of length I with Bayes Factors
-  #Z is a vector of length I with the initial guess for membership (1's or 0's)
-  ## Z can be NULL, in which case Z will be randomly sampled from a binomial with probabilities according to the Bayes Factor.
+Logit_FGEM <- function(u,x,B,useBayes=T){
   
-  #iters is the number of EM iterations.  If not specified, EM will be run untill convergence specified by tol.
-  
-  if(is.null(NullLogLikelihood)){
-    likfun <- function(x,B){
-      sum(log((1/(1+exp(-x)))*B+(1-(1/(1+exp(-x))))))
-    }
-    NullLogLikelihood  <- optimize(likfun,interval = c(-10,10),B=B,maximum=T)$objective
-  }
-  
-  if(is.null(tol)&is.null(iters)){
-    stop("Must specify tol or iters")
-  }
-  if(ifelse(is.null(Z),
-            length(x)!=length(B),
-            (length(x)!=length(B))|(length(x)!=length(Z))|(length(B)!=length(Z)))){
-    stop("Length of x and B (and Z, if specified) must be equal")
-  }
-  
-  if(is.null(Z)){
-    pB <- ecdf(B)(B)
-    sigcount <- sum(pB)/sum(B>10)
-    Z <- rbinom(length(B),size = 1,prob = pB/sigcount)
-  }
-  if(!is.null(iters)&is.null(tol)){
-    tol <- 0
-  }
-  if(!is.null(tol)&is.null(iters)){
-    iters <- 1000
-  }
-  if(keep){
-    matdim <- iters
-    Beta <- matrix(0,2,matdim)
-    Beta[,1] <- coefficients(glm(Z~x,family=binomial))
-    pvec <- 1/(1+exp(-(Beta[1,1]+Beta[2,1]*x)))
-    LogLikelihood <- numeric(matdim)
-    LogLikelihood[1] <- sum(log(pvec*B+(1-pvec)))
-    i <- 1
-    while ((i < iters)&(i<1000)){
-     #This is the mixture proportion
-      uvec  <- (pvec*B)/((pvec*B)+(1-pvec))
-      #This is where we optimize the Q function (in this case, using logistic regression)
-      Beta[,i] <- coefficients(glm(uvec~x,family=quasibinomial(link="logit")))
-      pvec  <- 1/(1+exp(-(Beta[1,i]+Beta[2,i]*x)))
-      #If the difference between the current log likelihood and the previous log likelihood is below the tolerance, we quit
-      rettol <- sum(log(pvec*B+(1-pvec)))-LogLikelihood[i]
-      if(rettol<tol){
-        LogLikelihood[i] <- sum(log(pvec*B+(1-pvec)))
-        break
-      }
-      LogLikelihood[i] <- sum(log(pvec*B+(1-pvec)))
-      #If we're about to stop and we still aren't better than the null model, we keep going (up to 1k iterations)
-      if(i==(iters-1)){
-        if(LogLikelihood[i]<NullLogLikelihood){
-          iters <- iters+10
-        }
-      }
-      i <- i+1
-    }
-    Chisq <- -2*(NullLogLikelihood-LogLikelihood[matdim])
-    retlist <- list(Intercept=Beta[1,matdim],Beta=Beta[2,matdim],
-                    LogLikelihood=LogLikelihood[matdim],
-                    NullLogLikelihood=NullLogLikelihood,
-                    Chisq=Chisq,tol=rettol)
-  }
-  else{
-    Beta <- coefficients(glm(Z~x,family=binomial))
-    pvec <- 1/(1+exp(-(Beta[1]+Beta[2]*x)))
-    LogLikelihood <- sum(log(pvec*B+(1-pvec)))
-    i <- 1
-    while ((i < iters)&(i<1000)){
-      #This is the mixture proportion
-      uvec  <- (pvec*B)/((pvec*B)+(1-pvec))
-      #This is where we optimize the Q function (in this case, using logistic regression)
-      Beta <- coefficients(glm(uvec~x,family=quasibinomial(link="logit")))
-      pvec  <- 1/(1+exp(-(Beta[1]+Beta[2]*x)))
-      #If the difference between the current log likelihood and the previous log likelihood is below the tolerance, we quit
-      rettol <- sum(log(pvec*B+(1-pvec)))-LogLikelihood
-      if(rettol<tol){
-        LogLikelihood <- sum(log(pvec*B+(1-pvec)))
-        break
-      }
-      LogLikelihood <- sum(log(pvec*B+(1-pvec)))
-      #If we're about to stop and we still aren't better than the null model, we keep going (up to 1k iterations)
-      if(i==(iters-1)&LogLikelihood<NullLogLikelihood){
-        iters <- iters+10
-      }
-      i <- i+1
-    }
-    Chisq <- -2*(NullLogLikelihood-LogLikelihood)
-    retlist <- list(Intercept=Beta[1],Beta=Beta[2],
-                    LogLikelihood=LogLikelihood,
-                    NullLogLikelihood=NullLogLikelihood,
-                    Chisq=Chisq,tol=rettol)
-  } 
-  if(keep){
-    mats <- list(Betas=Beta,LogLikelihood=LogLikelihood)
-    return(list(retlist,mats))
+  mx <- cbind(1,x)
+  if(useBayes){
+    Beta <- coefficients(bayesglm(u~x,family=quasibinomial(link="logit")))
   }else{
-    return(data.frame(retlist))
+    Beta <- coefficients(glm(u~x,family=quasibinomial(link="logit")))
   }
+  pvec  <- 1/(1+exp(-(mx%*%Beta)))
+  uvec <- (pvec*B)/((pvec*B)+(1-pvec))
+  return(uvec)
 }
 
 
-FGEM.df <- adply(GOmat,2,FGEM,B=B,iters=50,keep=F,NullLogLikelihood=NullLogLikelihood,.parallel=T)
-FGEM.df$pval <- pchisq(FGEM.df$Chisq,df = 1,lower.tail = F)
-FGEM.df <- FGEM.df[order(FGEM.df$pval),]
-FGEM.df$adjp <- p.adjust(FGEM.df$pval,method = "fdr")
+Logit_log_lik <- function(u,x,B,useBayes=T){
+    mx <- cbind(1,x)
+    if(useBayes){
+      Beta <- coefficients(bayesglm(u~x,family=quasibinomial(link="logit")))
+    }else{
+      Beta <- coefficients(glm(u~x,family=quasibinomial(link="logit")))
+    }
+    pvec  <- 1/(1+exp(-(mx%*%Beta)))
+    uvec <- (pvec*B)/((pvec*B)+(1-pvec))
+    return(-sum(log(uvec*(pvec+B)+(1-uvec)*(1-pvec))))
+}
+
+
+Beta_log_lik <- function(Beta,x,B){
+    mx <- cbind(1,x)
+#    Beta <- coefficients(glm(u~x,family=quasibinomial(link="logit")))
+    pvec  <- 1/(1+exp(-(mx%*%Beta)))
+    uvec <- (pvec*B)/((pvec*B)+(1-pvec))
+    return(-sum(log(uvec*(pvec+B)+(1-uvec)*(1-pvec))))
+}
+
+Logit.uvec <- apply(GOmat,2,function(x,B){
+    pf <- squarem(par=B/(B+exp(-B)),fixptfn=Logit_FGEM,
+                  objfn=Logit_log_lik,x=x,B=B)
+    return(pf$par)},B=B)
+exac.B <- squarem(par=tB/(tB+exp(-tB)),fixptfn=Logit_FGEM,
+                  objfn=Logit_log_lik,x=tEx$mis_z,B=tB)
+
+eB <- bayesglm(exac.B$par~tEx$mis_z,family=quasibinomial(link="logit"))
+Bmat <- matrix(0,ncol(Logit.uvec),4)
+colnames(Bmat) <- c("Intercept","Beta","Likelihood","Null")
+rownames(Bmat) <- colnames(Logit.uvec)
+
+for(i in 1:nrow(Bmat)){
+    tbeta <- gen_p(Logit.uvec[,i],GOmat[,i],B)
+    Bmat[i,"Intercept"] <- tbeta[1]
+    Bmat[i,"Beta"] <- tbeta[2]
+    Bmat[i,"Likelihood"] <- Beta_log_lik(tbeta,GOmat[,i],B)
+    Bmat[i,"Null"] <- Beta_log_lik(c(tbeta[1],0),GOmat[,i],B)
+}
+Bmat <- data.frame(Bmat)
+    
+
 print("Done!")
-saveRDS(FGEM.df,outfile)
+#rownames(Logit.uvec) <- rownames(GOmat)
+saveRDS(Bmat,outfile)
 
 
 
--- Generate_GOmat.R
+++ Generate_GOmat.R
@@ -10,7 +10,7 @@ library(Matrix)
 
 # BFile <- args[1]
 # outfile <- args[2]
-setwd("~/Dropbox/BayesianDA/FGEM/Data")
+setwd("./Data/")
 BFile <- "TADA_ASC_SSC_results_Dec23.csv"
 outfile <- "GOmat.RDS"
 BF.G <- read.table(BFile,header=T,sep=",")
@@ -19,32 +19,29 @@ BF.G$sig <- ifelse(BF.G$BF>10,1,0)
 
 allgenes <- data.frame(Gene=BF.G$Gene,B=BF.G$BF,stringsAsFactors=F)
 rownames(allgenes) <- allgenes$Gene
-allgenes$Gi <- as.integer(factor(allgenes$Gene))
+allgenes$Gi <- 1:nrow(allgenes)
+genelist <- allgenes$Gene
+
+genGOdf <- function(genelist,GOC){
+  GO.G <- melt(annFUN.org(GOC,feasibleGenes=genelist,mapping="org.Hs.eg.db",ID="symbol"))
+  colnames(GO.G) <- c("Gene","GOT")
+  return(GO.G)
+}
+#  GOBP.G <- melt(annFUN.org("BP",feasibleGenes=BF.G$Gene,mapping="org.Hs.eg.db",ID="symbol"))
+#colnames(GOBP.G) <- c("Gene","GOT")
 
 
-GOMF.G <- melt(annFUN.org("MF",feasibleGenes=BF.G$Gene,mapping="org.Hs.eg.db",ID="symbol"))
-colnames(GOMF.G) <- c("Gene","GOT")
-GOBP.G <- melt(annFUN.org("BP",feasibleGenes=BF.G$Gene,mapping="org.Hs.eg.db",ID="symbol"))
-colnames(GOBP.G) <- c("Gene","GOT")
-
-GO.G <- rbind(GOMF.G,GOBP.G)
-GO.G$Gi <- allgenes[GO.G$Gene,"Gi"]
-GO.G$GOi <- as.integer(factor(GO.G$GOT))
-
-
-GOmat <- sparseMatrix(i=GO.G$Gi,j = GO.G$GOi,x = rep(1,nrow(GO.G)),dimnames=list(as.character(allgenes$Gene),as.character(levels(factor(GO.G$GOT)))))
-GOmat <- GOmat[,colSums(GOmat)>3]
-
-B <- allgenes[rownames(GOmat),"B"]
+GO.G <- rbind(genGOdf(as.character(genelist),"BP"),genGOdf(as.character(genelist),"MF"))
+GOT <- data.frame(Gi=1:length(unique(GO.G$GOT)),GOT=unique(GO.G$GOT))
+rownames(GOT) <- GOT$GOT
+GO.G$Gi <- allgenes[as.character(GO.G$Gene),"Gi"]
+GO.G$GOi <- GOT[GO.G$GOT,"Gi"]
 
 
 
-APBeta <- numeric(ncol(GOmat))
-Bmat <- matrix(B,nrow=length(B),ncol=1)
-GOb <- Matrix(GOmat,forceCheck = T,sparse = F)
-GOb <- matrix(GOb,nrow(GOb),ncol(GOb))
-cB <- cor(GOb,B)^2
- 
-GOmat <- GOmat[,order(cB[,1],decreasing = T)]
+
+GOmat <- sparseMatrix(i=GO.G$Gi,j = GO.G$GOi,x=1,dims=c(nrow(allgenes),max(GO.G$GOi)),
+                       dimnames=list(as.character(allgenes$Gene),rownames(GOT)))
+
 
 saveRDS(GOmat,file=outfile)
