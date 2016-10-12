47289d8496503e0b5ee2bb8a73fd8b62bd0fd57c
Nicholas Knoblauch <nwknoblauch@gmail.com>
2016-10-12 12:52:17 -0500

    Analyze remaining features

--- R/FGEM_Func.R
+++ R/FGEM_Func.R
@@ -1,177 +1,24 @@
-
-
-
-#Given a sparse matrix, write it to hdf5 format
-# sparseAnnowrite <- function(h5f,)
-
-#Code to read sparse matrix annotations from an HDF5 file ()
-sparseAnnoread <- function(h5f,datapath,rowpath="GeneList/",colpath){
-  require(rhdf5)
-  require(Matrix)
-  rowname <- h5read(h5f,rowpath)
-  colname <- h5read(h5f,colpath)
-  dat <-h5read(h5f,datapath)
-  datmat <- sparseMatrix(i=dat[,1],j=dat[,2],x=1,
-                         dims=c(length(rowname),length(colname)),
-                         dimnames=list(rowname,colname))
-  return(datmat)
-}
-
-#Return a matrix with two columns,z_score, and Bayes Factor (genes are rownames)
-cons_to_bf <- function(consmat,allgenes){
-  require(dplyr)
-  consdf <- data_frame(Gene=rownames(consmat),mis_z=consmat[,"mis_z"])
-  acons <- inner_join(consdf,allgenes)
-  acm <- as.matrix(select(acons,-Gene))
-  rownames(acm) <- acons$Gene
-  return(acm)
-}
-
-
-
-GenPrior <- function(Beta,x){
-  mx <- cbind(1,x)
-  pvec <- 1/(1+exp(-(mx%*%Beta)))
-  return(pvec)
-}
-
-
-Logit_FGEM <- function(u,x,B){
-  mx <- cbind(1,x)
-  Beta <- coefficients(glm(u~x,family=quasibinomial(link="logit")))
-  pvec  <- 1/(1+exp(-(mx%*%Beta)))
+FGEM_Logit <- function(Beta,x,B){
+  pvec  <- 1/(1+exp(-(x%*%Beta)))
   uvec <- (pvec*B)/((pvec*B)+(1-pvec))
-  return(uvec)
-}
-
-B_Logit_FGEM <- function(Beta,x,B,isIntercept=F){
-  if(!isIntercept){
-    mx <- cbind(1,x)
-  }else{
-    mx <- cbind(x)
-  }
-  pvec  <- 1/(1+exp(-(mx%*%Beta)))
-  uvec <- (pvec*B)/((pvec*B)+(1-pvec))
-  if(!isIntercept){
-    Beta <- coefficients(glm(uvec~x,family=quasibinomial(link="logit")))
-  }else{
-    Beta <- coefficients(glm(uvec~x+0,family=quasibinomial(link="logit")))
-  }
+  Beta <- coefficients(glm(uvec~x+0,family=quasibinomial(link="logit")))
   return(Beta)
 }
 
-
-
-n_Beta_FGEM <- function(Beta,x,B,isIntercept=F){
-  # if(!isIntercept){
-  #   mx <- cbind(1,x)
-  # }else{
-  #   mx <- cbind(x)
-  # }
-  # pvec  <- 1/(1+exp(-(mx%*%Beta)))
-  # uvec <- (pvec*B)/((pvec*B)+(1-pvec))
-  if(!isIntercept){
-    np <- optim(par = Beta,fn = B_Logit_log_lik,x=x,B=B,isIntercept=F)
-    Beta <- np$par
-  }else{
-    np <- optimise(B_Logit_log_lik,interval = c(-20,20),x=rep(1,length(x)),B=B,isIntercept=T)
-    Beta <- np$minimum
-  }
-  Beta <- np$par
-
-  # if(!isIntercept){
-  #   Beta <- coefficients(glm(uvec~x,family=quasibinomial(link="logit")))
-  # }else{
-  #   Beta <- coefficients(glm(uvec~x+0,family=quasibinomial(link="logit")))
-  # }
-  # return(Beta)
-}
-
-B_Logit_log_lik <- function(Beta,x,B,isIntercept=F){
-  if(!isIntercept){
-    mx <- cbind(1,x)
-  }else{
-    mx <- cbind(x)
-  }
-  pvec  <- 1/(1+exp(-(mx%*%Beta)))
+FGEM_Logit_log_lik <- function(Beta,x,B){
+  pvec  <- 1/(1+exp(-(x%*%Beta)))
   # opvec  <- 1/(1+exp((mx%*%Beta)))
   uvec <- (pvec*B)/((pvec*B)+(1-pvec))
-  return(-sum(uvec*(log(pvec*B))+(1-uvec)*log(1-pvec)))
+  return(-sum(uvec*(log(pvec+B))+(1-uvec)*log(1-pvec)))
 }
-
-Flog_lik <- function(Beta,x,B,isIntercept=F){
-  if(!isIntercept){
-    mx <- cbind(1,x)
-  }else{
-    mx <- cbind(x)
-  }
-  pvec  <- 1/(1+exp(-(mx%*%Beta)))
-  uvec <- (pvec*B)/((pvec*B)+(1-pvec))
-  tmod <- glm(uvec~x,family=quasibinomial(link="logit"))
-  return(sum(log(pvec*B+(1-pvec))))
-}
-
-
-Logit_log_lik <- function(u,x,B){
-  mx <- cbind(1,x)
-  Beta <- coefficients(glm(u~x,family=quasibinomial(link="logit")))
-  pvec  <- 1/(1+exp(-(mx%*%Beta)))
-  # opvec  <- 1/(1+exp((mx%*%Beta)))
-  uvec <- (pvec*B)/((pvec*B)+(1-pvec))
-  return(-sum(uvec*log(pvec+B)+(1-uvec)*log(1-pvec)))
-}
-
-
-#
-# Beta_log_lik <- function(Beta,x,B,isIntercept=F){
-#   if(!isIntercept){
-#     mx <- cbind(1,x)
-#   }else{
-#     mx <- cbind(x)
-#   }
-#   pvec  <- 1/(1+exp(-(mx%*%Beta)))
-#   uvec <- (pvec*B)/((pvec*B)+(1-pvec))
-#   return(-sum(uvec*log(pvec+B)+(1-uvec)*log(1-pvec)))
-# }
 
 gen_p <- function(Beta,x){
-  mx <- cbind(1,x)
-  pvec  <- 1/(1+exp(-(mx%*%Beta)))
+  pvec  <- 1/(1+exp(-(x%*%Beta)))
   return(pvec)
 }
 gen_u <- function(Beta,x,B){
   p <- gen_p(Beta,x)
   return((p*B)/((p*B)+(1-p)))
-}
-
-Bayesfile <- function(BayesFactorFile){
-  BF.df <- read.table(BayesFactorFile,header=T,sep=",")
-  allgenes <- data.frame(Gene=BF.df$Gene,B=BF.df$BF,stringsAsFactors=F)
-  allgenes$Gene <- as.character(allgenes$Gene)
-  rownames(allgenes) <- allgenes$Gene
-  return(allgenes)
-}
-
-gen_final_mat <- function(X1,GOmatfile,Consfile,BayesFactorFile,expfeatfile=NULL){
-  gox <- X1[grepl("GO",X1)]
-  consx <- X1[!grepl("GO",X1)]
-  GOmat <- readRDS(GOmatfile)
-  consdat <- readRDS(Consfile)
-  consg <- rownames(consdat)
-  consdat <- as_data_frame(consdat) %>% select(one_of(X1)) %>% mutate(Gene=consg)
-allgenes <- Bayesfile(BayesFactorFile)
-  consdat <- inner_join(consdat,allgenes)
-  GOdf <- as_data_frame(data.frame(as.matrix(GOmat[,gox,drop=F]),check.names = F,stringsAsFactors = F)) %>% mutate(Gene=rownames(GOmat))
-  if(!is.null(expfeatfile)){
-    expdat <- readRDS(expfeatfile)
-    expg <- rownames(expdat)
-    expdf <- as_data_frame(expdat) %>% select(one_of(X1)) %>% mutate(Gene=expg)
-    consdf <- inner_join(expdf,consdat)
-  }
-  findf <- inner_join(consdf,GOdf)
-  fmat <- data.matrix(select(findf,-Gene,-B))
-  rownames(fmat) <- findf$Gene
-  return(list(fmat,B=findf$B))
 }
 
 anno2df <- function(annodf,feat.name="ExAC"){
@@ -181,18 +28,17 @@ anno2df <- function(annodf,feat.name="ExAC"){
   retdf <- gather(annodf,feature,value,-Gene) %>% mutate(class=feat.name)
 }
 
-
 exp2df<- function(expmat,feat.name="Expression"){
   require(dplyr)
   require(tidyr)
   Genes <-rownames(expmat)
-  expdf <- as_data_frame(expmat) %>% mutate(gene=Genes) %>% gather(feature,value,-gene) %>% mutate(class=feat.name)
+  expdf <- as_data_frame(expmat) %>% mutate(Gene=Genes) %>% gather(feature,value,-Gene) %>% mutate(class=feat.name)
   return(expdf)
 }
 
 glist2df <- function(Genes,feat.name){
   require(dplyr)
-  retdf <- data_frame(gene=Genes) %>% mutate(feature=feat.name,value=1,class=feat.name)
+  retdf <- data_frame(Gene=Genes) %>% mutate(feature=feat.name,value=1,class=feat.name)
   return(retdf)
 }
 
@@ -202,15 +48,13 @@ GO2df <-function(Genes,terms="BP"){
   require(dplyr)
   mdf <- melt(topGO::annFUN.org(terms,feasibleGenes=Genes,mapping="org.Hs.eg.db",ID="symbol"))
   mdf <- as_data_frame(mdf)
-  mdf <- dplyr::select(mdf,gene=value,feature=L1) %>% mutate(value=1,class=paste0("GO_",terms))
+  mdf <- dplyr::select(mdf,Gene=value,feature=L1) %>% mutate(value=1,class=paste0("GO_",terms))
   return(mdf)
 }
 
 
-
 cfeat_df <- function(annodf,datadf,impute=F){
   require(dplyr)
-  require(SQUAREM)
   stopifnot(length(unique(annodf$feature))==1)
    isbin <- all(annodf$value==1)
    if(isbin){
@@ -229,154 +73,72 @@ cfeat_df <- function(annodf,datadf,impute=F){
    return(full_feat)
 }
 
-pmean <-function(Beta,feat_mat,isIntercept=F){
-  if(!isIntercept){
-    mx <- cbind(1,feat_mat)
-  }else{
-    mx <- cbind(feat_mat)
-  }
-  pvec  <- 1/(1+exp(-(mx%*%Beta)))
+pmean <-function(Beta,feat_mat){
+  pvec  <- 1/(1+exp(-(feat_mat%*%Beta)))
   return(mean(pvec))
 }
 
 
-pem_df <-function(full_feat){
-  cat(length(unique(full_feat$feature)))
-  stopifnot(length(unique(full_feat$feature))!=1)
-  feat_mat <-select(full_feat,Gene,feature,value,BF) %>% spread(feature,value) %>% select(-Gene) %>% filter(complete.cases(.))
 
-  BF <-feat_mat$BF
-  tmu <-BF/(BF+exp(-BF))
-  feat_mat <- data.matrix(select(feat_mat,-BF))
-  fBeta <- coefficients(glm(tmu~feat_mat,family=quasibinomial(link="logit")))
-  opf <- squarem(par=fBeta,fixptfn=B_Logit_FGEM,
-                 objfn=B_Logit_log_lik,x=feat_mat,B=BF)
-  pBeta <- opf$par
-#  opf$par <- NULL
-  cn <- c(colnames(feat_mat))
-  nret <- data.frame(opf) %>% mutate(feature=c("Intercept",cn),value.objfn=-value.objfn) %>%
-    rename(LogLik=value.objfn,Beta=par)
-  nNullLogLik <- -Beta_log_lik(c(pBeta[1],rep(0,ncol(feat_mat))),feat_mat,BF)
-  prior_mean <- pmean(pBeta,feat_mat,isIntercept = T)
-  nret <- mutate(nret,NullLogLik=nNullLogLik,Chisq=2*(NullLogLik-LogLik),
-                 pval=pchisq(Chisq,df=1,lower.tail = F),prior_mean=prior_mean)
-  return(nret)
+EM_mat <-function(fBeta,feat_matrix,BF){
+  require(SQUAREM)
+  require(dplyr)
+  opf <- squarem(par=fBeta,fixptfn=FGEM_Logit,x=feat_matrix,B=BF)
+  cn <- colnames(feat_matrix)
+  opf$LogLik <- -FGEM_Logit_log_lik(opf$par,feat_matrix,BF)
+  retdf <-data.frame(opf) %>% select(-value.objfn) %>% mutate(Intercept=opf$par[1],feature=cn) %>% rename(Beta=par)
+  if(nrow(retdf)>1){
+    retdf <- filter(retdf,feature!="Intercept")
+  }
+  return(retdf)
 }
 
 
-Null_Intercept <-function(full_feat){
+sem_df <-function(full_feat){
   require(dplyr)
-  require(SQUAREM)
+
   require(tidyr)
-  stopifnot(length(unique(full_feat$feature))==1)
-  feat_mat <-select(full_feat,Gene,feature,value,BF) %>% mutate(value=1,feature="Intercept") %>% spread(feature,value) %>% select(-Gene)
+
+  feat_mat <-select(full_feat,Gene,feature,value,BF) %>% spread(feature,value)  %>% filter(complete.cases(.))
+  Genes <- select(feat_mat,Gene)
+  feat_mat <- mutate(Genes,Intercept=1) %>% inner_join(feat_mat) %>% select(-Gene)
+
   BF <-feat_mat$BF
-  tmu <-BF/(BF+exp(-BF))
+  tmu <-BF/(BF+exp(-(-5+1.5*log(BF))))
   feat_mat <- data.matrix(select(feat_mat,-BF))
   fBeta <- coefficients(glm(tmu~feat_mat+0,family=quasibinomial(link="logit")))
-  opf <- squarem(par=fBeta,fixptfn=n_Beta_FGEM,
-                 objfn=B_Logit_log_lik,x=feat_mat,B=BF,isIntercept=T)
 
-  pBeta <- opf$par
-  opf$par <- NULL
-  cn <- c(colnames(feat_mat))
-  nret <- data.frame(opf) %>% mutate(feature=cn,value.objfn=-value.objfn,Intercept=pBeta[1]) %>%
-    rename(LogLik=value.objfn)
-  nNullLogLik <- -B_Logit_log_lik(c(pBeta[1]),feat_mat,BF,isIntercept = T)
-  prior_mean <- pmean(pBeta,feat_mat,isIntercept = T)
-  nret <- mutate(nret,NullLogLik=nNullLogLik,prior_mean=prior_mean)
+  while(any(is.na(fBeta))){
+    feat_mat <- feat_mat[,!is.na(fBeta)[-1]]
+    fBeta <- coefficients(glm(tmu~feat_mat+0,family=quasibinomial(link="logit")))
+  }
+
+
+  retdf <- EM_mat(fBeta,feat_mat,BF) %>% mutate(nac=NA)
+  retdf <- EM_mat(fBeta[1],feat_mat[,"Intercept",drop=F],BF = BF) %>%
+    select(NullLogLik=LogLik) %>% mutate(nac=NA) %>%
+    inner_join(retdf) %>% select(-nac)
+  retdf <- mutate(retdf,Chisq=-2*(NullLogLik-LogLik),
+                  pval=pchisq(Chisq,df=ncol(feat_mat)-1,lower.tail=F))
+  return(retdf)
 }
 
 
 
 
-sem_df <-function(full_feat,NullIntercept=NULL){
-  require(dplyr)
-  require(SQUAREM)
-  require(tidyr)
-
-
-  cat(length(unique(full_feat$feature)))
-  stopifnot(length(unique(full_feat$feature))==1)
-  feat_mat <-select(full_feat,Gene,feature,value,BF) %>% spread(feature,value) %>% select(-Gene)
-  BF <-feat_mat$BF
-  tmu <-BF/(BF+exp(-(-4+0.1*BF)))
-  feat_mat <- data.matrix(select(feat_mat,-BF))
-  fBeta <- coefficients(glm(tmu~feat_mat,family=quasibinomial(link="logit")))
-
-  opf <- squarem(par=fBeta,fixptfn=n_Beta_FGEM,
-                 objfn=B_Logit_log_lik,x=feat_mat,B=BF)
-#  nopf <- squarem(par=fBeta,fixptfn=B_Logit_FGEM,objfn=B_Logit_log_lik,x=feat_mat,B=BF)
-  #  nopf <- squarem(par=fBeta,fixptfn=B_Logit_FGEM,x=feat_mat,B=BF)
-  pBeta <- opf$par
-  opf$par <- NULL
-  cn <- c(colnames(feat_mat))
-  nret <- data.frame(opf) %>% mutate(feature=cn,value.objfn=-value.objfn,Beta=pBeta[2],Intercept=pBeta[1]) %>%
-    rename(LogLik=value.objfn)
-  mus <-
-
-    if(is.null(NullIntercept)){
-      NullIntercept_df <-Null_Intercept(full_feat)
-      NullIntercept <- NullIntercept_df$Intercept
-    }
-  nNullLogLik <- B_Logit_log_lik(c(NullIntercept,0),feat_mat,BF)
-  nLogLik <- B_Logit_log_lik(pBeta,feat_mat,BF)
-  prior_mean <- pmean(pBeta,feat_mat)
-  nret <- mutate(nret,NullLogLik=nNullLogLik,LogLik=nLogLik,Chisq=2*(NullLogLik-LogLik),
-                 pval=pchisq(Chisq,df=1,lower.tail = F),prior_mean=prior_mean)
-  return(nret)
+chisq_comp <- function(full_feat,prior=0.02){
+  isbin <- length(unique(full_feat$value))==2
+  if(isbin){
+    return(chisq.test(full_feat$value,factor(quantile(full_feat$BF,prior))))
+  }
 }
 
-
-
-
-fisher_comp <- function(full_feat,prior=0.02){
-
-    feat_res <- group_by(full_feat,feature) %>% mutate(isbin=(length(unique(value))==2),
-                                           feat_bin=factor(ifelse(isbin,factor(value),value>quantile(value,prior))),
-                                           BF_bin=factor(BF>quantile(BF,prior))) %>%
-do(par=chisq.test(.$feat_bin,.$BF_bin,simulate.p.value = T)) %>% summarise(feature=feature[1],prior=prior,statistic=par$statistic,pval=par$p.value)
-    return(feat_res)
-}
-
-
-
-fullsem <- function(fmat,B){
-  pf <- squarem(par=B/(B+exp(-B)),fixptfn=Logit_FGEM,
-                objfn=Logit_log_lik,x=fmat,B=B)
-  return(pf$par)
-}
-
-compute_full_FGEM <-function(GOmatfile,Consfile,EXPfile,Bfile,GOresfiles){
-
-  resf <- c(Consfile,
-            GOresfiles,
-            "~/Dropbox/BayesianDA/FGEM/Data/Expression_Res.RDS")
-
-  BF.G <- read.table(Bfile,header=T,sep=",")
-  mres <- bind_rows(lapply(resf,readRDS))
-  mres <- mutate(mres,feature=ifelse(is.na(feature),X1,feature)) %>% select(-X1,-method)
-
-  mres <- mutate(mres,p=pchisq(Chisq,df=1,lower.tail = F),isGO=grepl("GO",feature)) %>% group_by(isGO) %>% mutate(p.adj=p.adjust(p,method="fdr")) %>% ungroup() %>% arrange(p.adj)
-
-  X1 <- filter(mres,p.adj<0.05)[["feature"]]
-  fml <- gen_final_mat(X1,GOmatfile,Consfile,Bfile,EXPfile)
-  fmlm <- fml[[1]]
-  fmlm <- scale(fmlm,center=T,scale=F)
-
-  par <- fullsem(fmlm,fml[[2]])
-  tbeta <- gen_p(par,fml[[1]],fml[[2]])
-  B <- fml$B
-  tgp <- GenPrior(tbeta,fml[[1]])
-  plot(tgp,log(B))
-  tpost <- (tgp*B)/(tgp*B+(1-tgp))
-  # plot(log(B),tpost)
-  fmldf <- as_data_frame(data.frame(fml[[1]],check.names=F))
-  fmldf <- mutate(fmldf,Gene=rownames(fml[[1]]),BF=B,prior=tgp,posterior=tpost)
-  fmldf <- mutate(fmldf,rank_change = rank(BF)-rank(posterior),prior=c(prior))
-  return(fmldf)
-}
-
+#     feat_res <- group_by(full_feat,feature) %>% mutate(isbin=(length(unique(value))==2),
+#                                            feat_bin=factor(ifelse(isbin,factor(value),value>quantile(value,prior))),
+#                                            BF_bin=factor(BF>quantile(BF,prior))) %>%
+# do(par=chisq.test(.$feat_bin,.$BF_bin,simulate.p.value = T)) %>% summarise(feature=feature[1],prior=prior,statistic=par$statistic,pval=par$p.value)
+#     return(feat_res)
+# }
 
 
 
--- analyses/Cluster_FGEM.R
+++ analyses/Cluster_FGEM.R
@@ -29,14 +29,14 @@ library(BBmisc)
 
 chunki <- as.integer(commandArgs(trailingOnly = T))
 cat(chunki,"\n")
-sub_all_anno_df<- readRDS("/scratch/midway/nwknoblauch/FGEM/sub_annotations.RDS")
+sub_all_anno_df<- readRDS("/scratch/midway/nwknoblauch/FGEM/remaining_features.RDS")
 datadf <- readRDS("/scratch/midway/nwknoblauch/FGEM/TADA.RDS")
 
 featureinds <- unique(sub_all_anno_df$feat_ind)
 cat("There are ",length(featureinds),"features\n")
 
 mchunk <- chunk(featureinds,chunk.size = 100)
-cat("There are ",length(mchunk)," chunks of size 20\n")
+cat("There are ",length(mchunk)," chunks of size 100\n")
 mchunk <- mchunk[[chunki]]
 miniter <- min(mchunk)
 maxiter <- max(mchunk)
@@ -45,6 +45,6 @@ s_anno_df <- filter(sub_all_anno_df,between(feat_ind,miniter,maxiter))
 
 results <- group_by(s_anno_df,feature) %>%  do(cfeat_df(.,datadf,impute=F)) %>%  do(sem_df(.)) %>% ungroup()
 
-saveRDS(results,paste0("/scratch/midway/nwknoblauch/FGEM/FGEM_Results/FGEM_",miniter,"_",maxiter,".RDS"))
+saveRDS(results,paste0("/scratch/midway/nwknoblauch/FGEM/FGEM_Results/rem_FGEM_",miniter,"_",maxiter,".RDS"))
 
 
